9b61533866837b8dad7c2e4de7ef0f229fb0b48a
AfeefRazick <afeefrazickamir@gmail.com>
2023-09-14 18:47:49 +0530

    setup devs page

---
+++ src/app/devs/page.tsx
@@ -0,0 +1,25 @@
+export default function Devs() {
+    return (
+        <div className="z-20 flex items-center" style={{ height: "calc(100vh - 8rem)" }}>
+            <div className="flex flex-col px-16">
+                <h1 className="text-5xl leading-tight uppercase text-neutral">
+                    MEET THE <br />
+                    DEVELOPERS
+                </h1>
+                <span className="mt-8 text-3xl leading-relaxed font-sans capitalize font-italic">
+                    Get to the know the ins and outs of the website <br /> and the team behind it
+                </span>
+
+                <div className="mt-24 inline-flex gap-8">
+                    <button className="h-10 rounded-xl bg-accent px-6 font-medium shadow">
+                        Join Us
+                    </button>
+                    <button className="h-10 inline-flex items-center">
+                        Learn More
+                        <div className="i-mdi-arrow-top-right mb-1 ml-1 h-4 w-4" />
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
