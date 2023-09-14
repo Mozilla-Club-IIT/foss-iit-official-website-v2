348c5aeb9af879f2a042e117a638b20a8c496101
Curstantine <curstantine@gmail.com>
2023-09-14 19:15:08 +0530

    fix: make social icons smaller to match the design

--- src/components/footer/SocialLink.tsx
+++ src/components/footer/SocialLink.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function SocialLink({ href, icon }: Props) {
     return (
         <div className="h-8 w-8 inline-flex items-center justify-center b-1 b-white rounded-full">
-            <div className={cn("h-6 w-6", icon)} />
+            <div className={cn("h-5 w-5", icon)} />
         </div>
     );
 }
