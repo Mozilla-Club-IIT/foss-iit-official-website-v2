8ab4de6eecfc2ee007c600d4531de3ad7fed0c9a
Curstantine <curstantine@gmail.com>
2023-09-15 19:14:10 +0530

    fix: lint errors

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -4,7 +4,7 @@ import NavLink from "@/components/nav/Link";
 
 export default function Nav() {
     return (
-        <nav className="containerify fixed inset-x-6 top-8 z-20 h-20 flex">
+        <nav className="fixed inset-x-6 top-8 z-20 h-20 flex containerify">
             <div className="h-20 w-full flex items-center rounded-2xl bg-black/[48%] pl-8 pr-12 ring-2 ring-border-nav/[4%] backdrop-blur-sm">
                 <Image src="/textual-logo.png" alt="Mozilla IIT" width={174} height={37} />
                 <div className="flex flex-1 justify-end gap-16 text-lg uppercase text-text-neutral">
