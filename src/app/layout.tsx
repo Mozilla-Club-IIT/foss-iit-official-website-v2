612e23aee5a40603b1ffb2c5c4133f171d835dc5
Curstantine <curstantine@gmail.com>
2023-09-15 13:23:02 +0530

    fix: scroll to top issue when navigating from pages.
    See: https://stackoverflow.com/questions/58008310/next-js-link-doesnt-render-with-page-scrolled-at-the-top/66316619#66316619

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en" className={`${poppins.variable}`}>
+        <html lang="en" className={`${poppins.variable} max-w-[100vw] overflow-x-hidden`}>
             <body className="relative h-[100vh] w-[100vw]">
                 <Nav />
                 <div className="absolute top-0 z-10 pt-32">
