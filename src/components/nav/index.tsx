1a3431712b1a443619cf504da47b6860cacac6fa
Curstantine <curstantine@gmail.com>
2023-09-16 17:24:00 +0530

    chore: smaller nav sizes for laptop widths

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -4,10 +4,16 @@ import NavLink from "@/components/nav/Link";
 
 export default function Nav() {
     return (
-        <nav className="fixed inset-x-6 top-8 z-20 h-20 flex containerify">
-            <div className="h-20 w-full flex items-center rounded-2xl bg-black/[48%] pl-8 pr-12 ring-2 ring-border-nav/[4%] backdrop-blur-sm">
-                <Image src="/textual-logo.png" alt="Mozilla IIT" width={174} height={37} />
-                <div className="flex flex-1 justify-end gap-16 text-lg uppercase text-text-neutral">
+        <nav className="fixed inset-x-2 top-4 z-20 h-16 flex containerify 2xl:inset-x-6 2xl:top-8 2xl:h-20">
+            <div className="h-16 w-full flex items-center rounded-2xl bg-black/[48%] pl-8 pr-12 ring-2 ring-border-nav/[4%] backdrop-blur-sm 2xl:h-20">
+                <Image
+                    src="/textual-logo.png"
+                    alt="Mozilla IIT"
+                    width={174}
+                    height={37}
+                    className="w-[145px] 2xl:w-fit"
+                />
+                <div className="flex flex-1 justify-end gap-16 text-base uppercase text-text-neutral 2xl:text-lg">
                     <NavLink href="/" label="Home" />
                     <NavLink href="/about" label="About" />
                     <NavLink href="/events" label="Events" />
