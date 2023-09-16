595381ad65c46328ca476c73cdb3a37beafad978
Curstantine <curstantine@gmail.com>
2023-09-16 16:52:15 +0530

    chore: moved all placeholder content to their own files

--- src/app/devs/page.tsx
+++ src/app/devs/page.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import type { FC } from "react";
 
-import type { Member } from "@/types/internal";
-
 import TechnologyCard from "@/components/cards/TechnologyCard";
 import AccentedButton from "@/components/buttons/AccentedButton";
 import TrailingIconButton from "@/components/buttons/TrailingIconButton";
 import HeroLayout from "@/components/hero/HeroLayout";
 import MemberCard from "@/components/cards/MemberCard";
+
+import { developers } from "@/constants/placeholders";
 
 export default function Devs() {
     return (
@@ -89,31 +89,3 @@ const Developers: FC = () => {
         </div>
     );
 };
-
-const developers: Member[] = [
-    {
-        name: "Sahan Dissanayake",
-        role: "UI/UX Designer",
-        imageURL: "/react-logo.png",
-    },
-    {
-        name: "Sahan Dissanayake",
-        role: "UI/UX Designer",
-        imageURL: "/react-logo.png",
-    },
-    {
-        name: "Sahan Dissanayake",
-        role: "UI/UX Designer",
-        imageURL: "/react-logo.png",
-    },
-    {
-        name: "Sahan Dissanayake",
-        role: "UI/UX Designer",
-        imageURL: "/react-logo.png",
-    },
-    {
-        name: "Sahan Dissanayake",
-        role: "UI/UX Designer",
-        imageURL: "/react-logo.png",
-    },
-];
--- src/constants/placeholders.ts
+++ src/constants/placeholders.ts
@@ -1,4 +1,4 @@
-import type { Event } from "@/types/internal";
+import type { Event, Member, WIFBoardOfficial } from "@/types/internal";
 
 export const event: Event = {
     name: "OpenHack 3.0",
@@ -13,3 +13,78 @@ export const events: Event[] = Array.from({ length: 5 }).map((_, i) => ({
     ...event,
     name: `OpenHack ${i}`,
 }));
+
+export const members: Record<0 | 1 | 2 | 3, Member | Member[]> = {
+    0: {
+        name: "Sahan Ekanayake",
+        role: "President - Mozzila Campus club of IIT",
+        imageURL: "",
+    },
+    1: {
+        name: "Sachith Arianatha",
+        role: "President - Mozilla Campus club of IIT",
+        imageURL: "",
+    },
+    2: [
+        { name: "Bhagya", role: "President - Mozilla Campus club of IIT", imageURL: "" },
+        { name: "Vinuja", role: "President - Mozilla Campus club of IIT", imageURL: "" },
+        { name: "Devmal", role: "President - Mozilla Campus club of IIT", imageURL: "" },
+    ],
+    3: [
+        { name: "Media Directors", role: "President - Mozilla Campus club of IIT", imageURL: "" },
+        { name: "Manupa Chathurwara", role: "Director of IIT", imageURL: "" },
+        { name: "Abrar", role: "Director Membership & Club Growth", imageURL: "" },
+        { name: "Shakthi ", role: "Editor", imageURL: "" },
+    ],
+};
+
+export const developers: Member[] = [
+    {
+        name: "Sahan Dissanayake",
+        role: "UI/UX Designer",
+        imageURL: "/react-logo.png",
+    },
+    {
+        name: "Sahan Dissanayake",
+        role: "UI/UX Designer",
+        imageURL: "/react-logo.png",
+    },
+    {
+        name: "Sahan Dissanayake",
+        role: "UI/UX Designer",
+        imageURL: "/react-logo.png",
+    },
+    {
+        name: "Sahan Dissanayake",
+        role: "UI/UX Designer",
+        imageURL: "/react-logo.png",
+    },
+    {
+        name: "Sahan Dissanayake",
+        role: "UI/UX Designer",
+        imageURL: "/react-logo.png",
+    },
+];
+
+export const wifBoard: WIFBoardOfficial[] = [
+    {
+        name: "Nadul Jayasundera",
+        imageURL: "/portraits/nadul.png",
+        externalLinks: { linkedin: "http://linkedin.com" },
+    },
+    {
+        name: "Nadul Jayasundera",
+        imageURL: "/portraits/nadul.png",
+        externalLinks: { linkedin: "http://linkedin.com" },
+    },
+    {
+        name: "Nadul Jayasundera",
+        imageURL: "/portraits/nadul.png",
+        externalLinks: { linkedin: "http://linkedin.com" },
+    },
+    {
+        name: "Nadul Jayasundera",
+        imageURL: "/portraits/nadul.png",
+        externalLinks: { linkedin: "http://linkedin.com" },
+    },
+];
