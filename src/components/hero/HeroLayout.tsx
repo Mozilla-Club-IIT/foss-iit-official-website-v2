5f89491546287e1dfaf8dfb39ef414fdbf2c4c1d
Curstantine <curstantine@gmail.com>
2023-09-17 08:20:08 +0000

    feat: hero layout and nav mobile sizes/layout

--- src/components/hero/HeroLayout.tsx
+++ src/components/hero/HeroLayout.tsx
@@ -9,12 +9,11 @@ type Props = {
 };
 
 export default function HeroLayout({ title, subtitle, children }: Props) {
+    const newLocal = "text-3xl md:text-5xl leading-tight uppercase text-text-neutral";
     return (
-        <div className="flex flex-col px-16">
-            <h1 className="text-5xl leading-tight uppercase text-text-neutral">
-                {Array.isArray(title) ? mapToLines(title) : title}
-            </h1>
-            <span className="mt-8 text-lg font-medium font-italic">
+        <div className="flex flex-col md:px-16">
+            <h1 className={newLocal}>{Array.isArray(title) ? mapToLines(title) : title}</h1>
+            <span className="text-md mt-8 font-medium font-italic md:text-lg">
                 {Array.isArray(subtitle) ? mapToLines(subtitle) : subtitle}
             </span>
             <div className="mt-24 inline-flex gap-8">{children}</div>
--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -2,25 +2,33 @@ import Image from "next/image";
 
 import NavLink from "@/components/nav/Link";
 
+import textualLogo from "public/textual-logo.png";
+import dotLogo from "public/dot-logo.png";
+
 export default function Nav() {
     return (
-        <nav className="fixed inset-x-6 top-6 z-20 h-16 flex px-0 containerify 2xl:top-8 2xl:h-20">
+        <nav className="fixed inset-x-0 top-6 z-20 mx-6 h-16 flex px-0 containerify 2xl:top-8 2xl:h-20">
             <div className="h-16 w-full flex items-center rounded-2xl bg-black/[48%] pl-8 pr-12 ring-2 ring-border-nav/[4%] backdrop-blur-sm 2xl:h-20">
                 <Image
-                    src="/textual-logo.png"
+                    src={textualLogo}
                     alt="Mozilla IIT"
-                    width={174}
-                    height={37}
-                    className="w-[145px] 2xl:w-fit"
+                    className="hidden w-[145px] md:block 2xl:w-fit"
                 />
-                <div className="flex flex-1 justify-end gap-16 text-base uppercase text-text-neutral 2xl:text-lg">
+                <Image
+                    src={dotLogo}
+                    width={32}
+                    height={32}
+                    alt="Mozilla IIT"
+                    className="md:hidden"
+                />
+                <div className="flex flex-1 justify-end gap-6 text-base uppercase text-text-neutral lg:gap-16 md:gap-12 2xl:text-lg">
                     <NavLink href="/" label="Home" />
                     <NavLink href="/about" label="About" />
                     <NavLink href="/events" label="Events" />
                     <NavLink href="/wif" label="WIF" />
                 </div>
             </div>
-            <div className="block h-20 w-42"></div>
+            <div className="hidden h-20 w-42 lg:block"></div>
         </nav>
     );
 }
