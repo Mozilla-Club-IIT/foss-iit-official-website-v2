85bd633a936dd41106b865f50794a075c63abc76
Curstantine <curstantine@gmail.com>
2023-09-19 15:05:40 +0530

    feat: events/string

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,4 +1,5 @@
-import { FC } from "react";
+import type { FC } from "react";
+import Image from "next/image";
 
 import AccentedButton from "@/components/buttons/AccentedButton";
 import TrailingIconButton from "@/components/buttons/TrailingIconButton";
@@ -7,6 +8,8 @@ import EventTimeLine from "@/components/trees/EventTimeLine";
 import EventCard from "@/components/cards/EventCard";
 
 import { events } from "@/constants/placeholders";
+
+import RedString from "public/onlygodknowswhatthisis.png";
 
 export default function Events() {
     return (
@@ -20,7 +23,7 @@ export default function Events() {
 
 const Hero: FC = () => {
     return (
-        <div className="flex items-center" style={{ height: "calc(100vh - 8rem)" }}>
+        <div className="relative flex items-center" style={{ height: "calc(100vh - 8rem)" }}>
             <HeroLayout
                 title="Our Events"
                 subtitle={[
@@ -33,6 +36,9 @@ const Hero: FC = () => {
                 <AccentedButton label="Latest Events" />
                 <TrailingIconButton icon="i-mdi-arrow-top-right" label="All Events" />
             </HeroLayout>
+            <div className="absolute hidden w-[calc(100%+12rem)] -right-24 -top-8 lg:block">
+                <Image src={RedString} alt="" />
+            </div>
         </div>
     );
 };
