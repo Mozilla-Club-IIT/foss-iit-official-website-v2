09754b6e32682d6fcb6189666e1f48c774723c9b
Curstantine <curstantine@gmail.com>
2023-09-19 16:28:01 +0530

    fix: nav logo sizing

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -10,7 +10,7 @@ export default function Nav() {
             style={{ maxWidth: "calc(100% - 3rem)" }}
         >
             <div className="h-16 flex items-center justify-center rounded-2xl bg-black/[48%] p-0 ring-2 ring-border-nav/[4%] backdrop-blur-sm 2xl:h-20 sm:justify-start xs:px-8 sm:pl-8 containerify">
-                <TextualLogo className="hidden w-[145px] md:block 2xl:w-fit" />
+                <TextualLogo className="hidden w-[145px] md:block" />
                 <DotLogo className="hidden h-8 w-8 xs:block md:hidden" />
                 <div className="flex justify-end gap-4 text-sm uppercase text-text-neutral xs:flex-1 lg:gap-16 md:gap-12 2xl:text-lg md:text-base">
                     <NavLink href="/" label="Home" />
