3bd38c2f3643eab7bb080b28c4aacf5e7c51485f
Curstantine <curstantine@gmail.com>
2023-09-19 11:21:45 +0530

    feat: Grid sheet impl

--- src/components/background/index.tsx
+++ src/components/background/index.tsx
@@ -1,16 +1,11 @@
+import GradientSheet from "@/components/background/GradientSheet";
+import Grids from "@/components/background/Grids";
+
 export default function Background() {
     return (
         <div className="fixed inset-0 w-full overflow-hidden -z-10">
-            <div className="absolute inset-0 bg-[#1C4762]" />
-
-            <div className="absolute right-96 h-256 w-36 rotate-48 bg-[#4C3033] blur-3xl -top-56" />
-            <div className="absolute right-96 h-256 w-36 rotate-48 bg-[#4C3033] blur-3xl -top-56" />
-
-            <div className="absolute h-[120vh] w-64 rotate-4 bg-[#632D1C] blur-3xl -left-12 -top-12" />
-            <div className="absolute h-128 w-128 rounded-full bg-[#571E20] blur-3xl -right-12 -top-12" />
-            <div className="absolute right-24 h-128 w-128 rounded-full bg-[#571E20] blur-3xl -bottom-64" />
-
-            <div className="absolute inset-0 bg-black/75" />
+            <GradientSheet />
+            <Grids />
         </div>
     );
 }
