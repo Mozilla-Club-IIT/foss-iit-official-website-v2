33ca710aebc5d45bb9c17aca8bd348685776f922
Curstantine <curstantine@gmail.com>
2023-09-20 12:03:36 +0530

    chore: update reg and social links

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,5 +1,6 @@
-export const FACEBOOK = "";
-export const TIKTOK = "";
-export const INSTAGRAM = "";
-export const LINKEDIN = "";
-export const REGISTER = "";
+export const FACEBOOK = "https://www.facebook.com/IITMozillaCampusClub";
+export const TIKTOK = "https://www.tiktok.com/@mozillacampusclubiit";
+export const INSTAGRAM = "https://www.instagram.com/mozilla_iit/";
+export const LINKEDIN = "https://lk.linkedin.com/company/mozilla-campus-club-iit";
+export const REGISTER =
+    "https://docs.google.com/forms/d/e/1FAIpQLSejM85topRl3zybhT9O8W2dK6hxwG5wJVsQ95Jacw0WMDH5NA/viewform";
