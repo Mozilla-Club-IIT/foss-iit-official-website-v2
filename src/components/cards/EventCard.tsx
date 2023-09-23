c350084edd3738bc3c6b3c0c251c757555c1ff62
Curstantine <curstantine@gmail.com>
2023-09-23 11:18:57 +0530

    chore: update event card to use better quality images

--- src/components/cards/EventCard.tsx
+++ src/components/cards/EventCard.tsx
@@ -10,8 +10,9 @@ export default function EventCard({ name, imageURL }: Props) {
     return (
         <div className="w-full flex flex-col items-center justify-between rounded-xl bg-white/10">
             <Image
-                height={256}
-                width={256}
+                height={512}
+                width={512}
+                quality={100}
                 src={imageURL}
                 alt={`${name} picture`}
                 className="aspect-square w-full rounded-t-xl object-cover"
