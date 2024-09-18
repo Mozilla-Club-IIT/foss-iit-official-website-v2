a6465c7c2abc117a88ad31a9f22f63cc52230e33
Curstantine <curstantine@proton.me>
2024-09-18 14:17:23 +0530

    chore: change registration URL

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -2,4 +2,4 @@ export const FACEBOOK = "https://www.facebook.com/IITMozillaCampusClub";
 export const TIK_TOK = "https://www.tiktok.com/@mozillacampusclubiit";
 export const INSTAGRAM = "https://www.instagram.com/mozilla_iit/";
 export const LINKEDIN = "https://lk.linkedin.com/company/mozilla-campus-club-iit";
-export const REGISTER = "https://forms.gle/XsokJTpsMavyKfVm9";
+export const REGISTER = "https://linktr.ee/Mozilla_Club_IIT";
