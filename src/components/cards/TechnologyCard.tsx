dd2ba7a2495c115c3ca0a4829f2305800fcf5b22
Curstantine <curstantine@proton.me>
2025-01-30 01:01:48 +0530

    chore: update dev page data

--- src/components/cards/TechnologyCard.tsx
+++ src/components/cards/TechnologyCard.tsx
@@ -5,7 +5,7 @@ type Props = Technology;
 export default function TechnologyCard({ name, icon }: Props) {
     return (
         <div className="h-36 w-36 flex flex-col items-center justify-evenly rounded-2xl bg-white/10 lg:h-48 lg:w-48">
-            <div className={clsx("h-20 w-20 lg:h-28 lg:w-28", icon)} />
+            <div className={clsx("size-16 lg:h-24 lg:w-24", icon)} />
             <h6 className="text-center">{name}</h6>
         </div>
     );
