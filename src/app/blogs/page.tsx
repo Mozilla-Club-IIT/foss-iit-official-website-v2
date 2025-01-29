6f50dc78d595f8dfb14b7b4ab1b586d35dcfed79
Curstantine <curstantine@proton.me>
2025-01-30 00:37:57 +0530

    feat: migrate blogs to prismic, add multi credit support and shiki

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -4,6 +4,7 @@ import { getPostsData } from "@/utils/blog";
 
 import BlogCard from "@/components/cards/BlogCard";
 import HeroLayout from "@/components/hero/HeroLayout";
+import { createClient } from "@/prismicio";
 
 export default function Blogs() {
     return (
@@ -16,7 +17,10 @@ export default function Blogs() {
 
 const Hero: FC = () => {
     return (
-        <div className="relative h-screen max-h-5xl flex items-center justify-between" style={{ height: "calc(100vh - 8rem)" }}>
+        <div
+            className="relative h-screen max-h-5xl flex items-center justify-between"
+            style={{ height: "calc(100vh - 8rem)" }}
+        >
             <HeroLayout
                 title="OUR BLOGS"
                 subtitle={["EXPLORE THE LATEST INSIGHTS AND TRENDS", "IN UI/UX DESIGN"]}
@@ -26,13 +30,38 @@ const Hero: FC = () => {
 };
 
 const BlogsList: FC = async () => {
-    const posts = await getPostsData();
+    const client = createClient();
+    const blogs = await client.getAllByType("blog", {
+        graphQuery: `{
+                blog {
+                    title
+                    description
+                    publication_date
+                    cover_image
+                    tags
+                    authors
+                }
+            }`,
+        orderings: [
+            { field: "blog.publication_date", direction: "desc" },
+        ],
+    });
+
+    console.log(blogs);
 
     return (
         <ul className="grid grid-cols-1 mb-24 gap-6 sm:grid-cols-[repeat(auto-fill,24rem)]">
-            {posts.map((blog) => (
+            {blogs.map((blog) => (
                 <li key={blog.id}>
-                    <BlogCard data={blog} />
+                    <BlogCard
+                        id={blog.uid}
+                        title={blog.data.title}
+                        cover_image={blog.data.cover_image}
+                        publication_date={blog.data.publication_date}
+                        authors={blog.data.authors}
+                        tags={blog.data.tags}
+                        description={blog.data.description}
+                    />
                 </li>
             ))}
         </ul>
