fcce39b0bd56180193816093313800eac1281056
Curstantine <Curstantine@proton.me>
2026-04-05 19:03:17 +0530

    chore: downgrade eslint to 9.39.4, update lockfile and simplify eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,18 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-import { FlatCompat } from "@eslint/eslintrc";
-import unocss from "@unocss/eslint-config/flat";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-    { ignores: ["**/dist", "**/.next"] },
-    ...compat.extends("next/core-web-vitals", "next/typescript"),
-    unocss,
-];
+const eslintConfig = defineConfig([
+    ...nextVitals,
+    ...nextTs,
+    // Override default ignores of eslint-config-next.
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        ".next/**",
+        "out/**",
+        "build/**",
+        "next-env.d.ts",
+    ]),
+]);
 
 export default eslintConfig;
